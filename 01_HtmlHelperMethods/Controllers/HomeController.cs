32de35086ded9086d57cd3d78cdbb85c3417014a
Vaso <ggo24@yahoo.com>
2019-09-16 17:23:51 +0400

    Add project files.

---
+++ 01_HtmlHelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_HtmlHelperMethods.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewBag.WeekDays = new string[] {"Monday", "Tuesday", "Wednesday"};
+            ViewBag.Fruits = new string[] { "Mango", "Kiwi", "Cinamon" };
+            return View();
+        }
+    }
+}
