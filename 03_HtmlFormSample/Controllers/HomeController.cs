a0835c52eb60fdb20ea67c54460a28f51aa17eb7
vasiliv <ggo24@yahoo.com>
2019-09-17 15:32:50 +0400

    03_HtmlFormSample

---
+++ 03_HtmlFormSample/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using _03_HtmlFormSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03_HtmlFormSample.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create() {
+            TaskModel model = new TaskModel();
+            model.Name = "Vasil";
+            model.Completed = false;
+            model.StartDate = DateTime.Now.ToShortDateString();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Create(TaskModel incomingData) {
+            Debug.WriteLine("Name = " + incomingData.Name);
+            Debug.WriteLine("StartDate = " + incomingData.StartDate);
+            Debug.WriteLine("Completed = " + incomingData.Completed);
+            return View("Success");
+        }
+    }
+}
